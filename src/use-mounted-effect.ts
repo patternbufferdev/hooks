0c4b182fd53fce87349fdd24b3a2c1ecbeb0017b
evanrs <me@evanrs.com>
2021-09-07 20:19:54 -0400

    feat: add support for effect runs

--- src/use-mounted-effect.ts
+++ src/use-mounted-effect.ts
@@ -1,16 +1,43 @@
 import { useEffect, DependencyList } from "react";
 
+type Effect = (run: Condition | (() => Destructor)) => Destructor;
+type Condition = () => boolean;
+type Operation = (condition: Condition) => Destructor;
+type Destructor = void | (() => void);
+
 export function useMountedEffect(
-  effect: (isMounted: () => boolean) => unknown | (() => void),
+  effect: (isMounted: Condition) => Destructor,
   dependencies?: DependencyList,
 ) {
   useEffect(() => {
     let mounted = true;
     let destructor = effect(() => mounted);
-    // TODO only return unmount when destructor not undefined?
+
     return () => {
       mounted = false;
-      if (typeof destructor === "function") destructor();
+      destructor?.();
     };
   }, dependencies);
+}
+
+export function useEffectGate(effect: Effect, dependencies?: DependencyList) {
+  return useMountedEffect((isMounted) => effect(onCondition(isMounted)), dependencies);
+}
+
+export function useMountedEffectRun(effect: Effect, dependencies?: DependencyList) {
+  useEffect(() => {
+    let mounted = true;
+    const condition = () => mounted;
+    const destructor = effect(onCondition(condition));
+
+    return () => {
+      mounted = false;
+      destructor?.();
+    };
+  }, dependencies);
+}
+
+function onCondition(condition: Condition) {
+  return (operation?: Operation) =>
+    typeof operation === "function" ? () => condition() && operation(condition) : condition;
 }
