6df15864d1b5425ef217c458571114ee303bd45e
evanrs <me@evanrs.com>
2021-09-08 11:24:53 -0400

    refactor: simplify use-mounted-effect

--- src/use-mounted-effect.ts
+++ src/use-mounted-effect.ts
@@ -1,34 +1,16 @@
 import { useEffect, DependencyList } from "react";
+// TODO: mounted.run(effect)
+// import { useMounted } from "./use-mounted";
 
 type Effect = (run: Condition | (() => Destructor)) => Destructor;
 type Condition = () => boolean;
 type Operation = (condition: Condition) => Destructor;
 type Destructor = void | (() => void);
 
-export function useMountedEffect(
-  effect: (isMounted: Condition) => Destructor,
-  dependencies?: DependencyList,
-) {
+export function useMountedEffect(effect: Effect, dependencies?: DependencyList) {
   useEffect(() => {
     let mounted = true;
-    let destructor = effect(() => mounted);
-
-    return () => {
-      mounted = false;
-      destructor?.();
-    };
-  }, dependencies);
-}
-
-export function useEffectGate(effect: Effect, dependencies?: DependencyList) {
-  return useMountedEffect((isMounted) => effect(onCondition(isMounted)), dependencies);
-}
-
-export function useMountedEffectRun(effect: Effect, dependencies?: DependencyList) {
-  useEffect(() => {
-    let mounted = true;
-    const condition = () => mounted;
-    const destructor = effect(onCondition(condition));
+    const destructor = effect(onCondition(() => mounted));
 
     return () => {
       mounted = false;
